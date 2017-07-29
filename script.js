cc88325811e06893a2920df684f5a5044bc244ca
dhananjayppatankar <dhananjayppatankar@gmail.com>
2017-07-28 23:41:30 -0400

    weather api

---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+
+    $("button").click(function(){
+      return getWeather();
+
+
+    });
+});
+
+function getWeather(){
+
+  var city = $("#submitcity").val();
+
+  if(city != ''){
+    $.ajax({
+      url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&APPID=2dacb6406f3fff1faa1612f018938fe5",
+      type: "GET",
+        datatype:"json",
+      success: function(data){
+        console.log(data);
+        var results =  showResults(data)
+        $("#showWeather").html(results);
+      }
+
+    });
+
+  }else {
+
+      $("#error").html("<div> City field cannot be empty </div>");
+  }
+
+
+}
+
+function showResults(data){
+  return '<h3>Current Weather for '+data.name +', ' +data.sys.country + '</h3>'+
+          '<p> Weather : ' + data.weather[0].main + '</p>';
+
+}
